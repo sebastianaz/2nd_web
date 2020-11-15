61fdb36a4095947d3c0d40442326f6037273bd00
sebastianaz <sebastian.azabala@gmail.com>
2020-11-15 08:49:41 -0500

    second commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const indexRoutes = require('./routes/index.js');
 
+app.set('port',process.env.PORT||3000);
+app.set('views',path.join(__dirname,'views'));
+app.engine('html',require('ejs').renderFile);
+app.set('view engine','ejs');
+
+//routes
+app.use(indexRoutes)
+//static files
+app.use(express.static(path.join(__dirname,'public')));
+
+app.listen(app.get('port'),()=>{
+    console.log('server on port ',app.get('port'))
+})
